38baaa2d654b13da2dc8be8121873211b9b324f8
edboma <123423046+edboma@users.noreply.github.com>
2025-07-29 23:20:17 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ function mostrar(id) {
   document.querySelectorAll('.pantalla').forEach(p => p.classList.remove('activa'));
   document.getElementById(id).classList.add('activa');
 }
+
+function init() {
+  tsParticles.load("confetti-bg", {
+    fullScreen: { enable: true, zIndex: 1 },
+    particles: {
+      number: { value: 80 },
+      shape: { type: "circle" },
+      color: { value: ["#FFC700", "#FF0000", "#2E3192", "#41BBC7"] },
+      opacity: { value: 0.9 },
+      size: { value: 5 },
+      move: { enable: true, direction: "bottom", speed: 4 }
+    }
+  });
+}
