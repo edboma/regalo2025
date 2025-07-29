b3240caac41dec894b8e287c88a38bd84dbfe63c
edboma <123423046+edboma@users.noreply.github.com>
2025-07-29 23:16:26 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-
-function mostrar(pantallaId) {
+function mostrar(id) {
   document.querySelectorAll('.pantalla').forEach(p => p.classList.remove('activa'));
-  document.getElementById(pantallaId).classList.add('activa');
-
-  // Control dinámico del menú
-  const menu = document.getElementById('menu');
-  menu.innerHTML = '';
-  if (pantallaId === 'comidas') {
-    menu.innerHTML += '<button onclick="mostrar(\'inicio\')">Inicio</button>';
-    menu.innerHTML += '<button onclick="mostrar(\'actividades\')">Actividades</button>';
-  } else if (pantallaId === 'actividades') {
-    menu.innerHTML += '<button onclick="mostrar(\'inicio\')">Inicio</button>';
-    menu.innerHTML += '<button onclick="mostrar(\'comidas\')">Comidas</button>';
-  } else {
-    menu.innerHTML += '<button onclick="mostrar(\'comidas\')">Comidas</button>';
-    menu.innerHTML += '<button onclick="mostrar(\'actividades\')">Actividades</button>';
-  }
+  document.getElementById(id).classList.add('activa');
 }
